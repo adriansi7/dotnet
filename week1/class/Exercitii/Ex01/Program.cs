779343641de2aaec5a127c258c6ecdaefb1da18f
adrian.scurtu <$M]*,62cL,6Y\gCn>
2020-03-06 15:58:24 +0200

    Ex01 Reverse Letters

--- week1/class/Exercitii/Ex01/Program.cs
+++ week1/class/Exercitii/Ex01/Program.cs
@@ -6,7 +6,28 @@ namespace Ex01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            reverseLetters();
+        }
+
+        static void reverseLetters()
+        {
+            int lettersCount;
+
+            Console.WriteLine("\nHow many letters?");
+
+            lettersCount = Convert.ToInt32(Console.ReadLine());
+
+            char[] letters = new char[lettersCount];
+
+            for (int i = 0; i < lettersCount; i++)
+            {
+                Console.WriteLine("\nEnter letter {0}", i+1);
+                letters[i] = Console.ReadKey().KeyChar;
+            }
+
+            Array.Reverse(letters);
+
+            Console.WriteLine("\nReversed array is {0}", string.Join(", ", letters));
         }
     }
 }
