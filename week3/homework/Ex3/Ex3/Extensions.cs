39ca84788bd9ae4f5c5e8dada0d8f95788f15676
adrian.scurtu <$M]*,62cL,6Y\gCn>
2020-03-20 19:52:32 +0200

    Homework 3 Ex 3

---
+++ week3/homework/Ex3/Ex3/Extensions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex3
+{
+    public static class Extensions
+    {
+        public static dynamic Sum<T>(this IEnumerable<T> elements)
+        {
+            dynamic sum = default(T);
+
+            foreach (T item in elements)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+
+        public static dynamic Product<T>(this IEnumerable<T> elements)
+        {
+            dynamic product = 1;
+
+            foreach (T item in elements)
+            {
+                product *= item;
+            }
+
+            return product;
+        }
+
+        public static dynamic Min<T>(this IEnumerable<T> elements)
+        {
+            dynamic min = long.MaxValue;
+
+            foreach (T item in elements)
+            {
+                if (item < min)
+                {
+                    min = item;
+                }
+            }
+
+            return min;
+        }
+
+        public static dynamic Max<T>(this IEnumerable<T> elements)
+        {
+            dynamic max = long.MinValue;
+
+            foreach (T item in elements)
+            {
+                if (item > max)
+                {
+                    max = item;
+                }
+            }
+
+            return max;
+        }
+
+        public static dynamic Average<T>(this IEnumerable<T> elements)
+        {
+            int count = 0;
+
+            foreach (T item in elements)
+            {
+                count++;
+            }
+
+            return elements.Sum() / count;
+        }
+    }
+}
---
+++ week3/homework/Ex3/Ex3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>() { 2, 3, 7, 9, 1 };
+
+            Console.WriteLine("Sum: {0}", intList.Sum());
+            Console.WriteLine("Product: {0}", intList.Product());
+            Console.WriteLine("Min: {0}", intList.Min());
+            Console.WriteLine("Max: {0}", intList.Max());
+            Console.WriteLine("Average: {0}", intList.Average());
+        }
+    }
+}
