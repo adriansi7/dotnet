2c71687d280133edc29dabbc4c74cc32eca24c23
adrian.scurtu <$M]*,62cL,6Y\gCn>
2020-03-20 16:18:55 +0200

    Homework 3 Ex 2

---
+++ week3/homework/Ex2/Ex2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Timers;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Timer x = new Timer(1);
+            x.AlarmEvent += processEvent;
+
+            while (true)
+            {
+                x.Add(1);
+                Thread.Sleep(3000);
+            }
+            
+        }
+        static void processEvent(object sender, EventArgs e)
+        {
+            Console.WriteLine("I am processing the event.");            
+        }
+    }
+}
---
+++ week3/homework/Ex2/Ex2/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex2
+{
+    class Timer
+    {
+        private int threshold;
+        private int total;
+        public event EventHandler AlarmEvent;
+
+        public Timer(int Threshold)
+        {
+            this.threshold = Threshold;
+        }
+
+        public void Add(int x)
+        {
+            this.total += x;
+
+            if (this.total >= this.threshold)
+            {
+                OnThresholdReached(EventArgs.Empty);
+            }
+        }
+
+        protected virtual void OnThresholdReached(EventArgs e)
+        {
+            EventHandler handler = AlarmEvent;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+    }
+}
+
