2b5cb4c7b8bbadebb3ef92afe972e9b40ea3f38f
adrian.scurtu <$M]*,62cL,6Y\gCn>
2020-03-23 21:08:18 +0200

    Homework 3 Ex 4

---
+++ week3/homework/Ex4/Ex4/InvalidRangeException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex4
+{
+    [Serializable]
+    class InvalidRangeException<T> : Exception
+    {
+        private static readonly string DefaultMessage = "The specified value is in an invalid range";
+
+        public T Start { get; set; }
+        public T End { get; set; }
+        public InvalidRangeException() : base(DefaultMessage) { }
+        public InvalidRangeException(string message) : base(message) { }
+        public InvalidRangeException(string message, Exception innerException): base(message, innerException) { }
+        public InvalidRangeException(T start, T end, string message) : base(message) 
+        {
+            this.Start = start;
+            this.End = end;
+        }
+
+    }
+}
---
+++ week3/homework/Ex4/Ex4/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            intException();
+            dateException();
+        }
+
+        static void intException()
+        {
+            Console.Write("Enter number: ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            string message = "Please enter a number between 1 and 100";
+
+            InvalidRangeException<int> intException = new InvalidRangeException<int>(1, 100, message);
+
+            try
+            {
+                if (number < intException.Start || number > intException.End)
+                {
+                    throw intException;
+                }
+                else
+                {
+                    Console.WriteLine("The number is in the range!");
+                }
+            }
+            catch (InvalidRangeException<int> ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Error, the valid range is {0} - {1}", ex.Start, ex.End);
+            }
+        }
+
+        static void dateException()
+        {
+            Console.Write("Enter a date (dd.MM.yyyy): ");
+
+            CultureInfo culture = new CultureInfo("ro-RO");
+
+            DateTime date = Convert.ToDateTime(Console.ReadLine(), culture);
+
+            string message = "Please enter a date between 1.1.1980 and 31.12.2013";
+
+            InvalidRangeException<DateTime> dateException = new InvalidRangeException<DateTime>(new DateTime(1980, 1, 1), new DateTime(2013, 12, 31), message);
+
+            try
+            {
+                if (date < dateException.Start || date > dateException.End)
+                {
+                    throw dateException;
+                }
+                else
+                {
+                    Console.WriteLine("The date is in the range!");
+                }
+            }
+            catch (InvalidRangeException<DateTime> ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Error, the valid range is {0} - {1}", ex.Start, ex.End);
+            }
+        }
+    }
+}
