5a4e58ed5cfc6139aaefc7eb6f49aeff1d8f6d06
adrian.scurtu <$M]*,62cL,6Y\gCn>
2020-03-24 14:51:37 +0200

    Homework 3 Ex 6

---
+++ week3/homework/Ex6/Ex6/BitArray64.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex6
+{
+    class BitArray64 : IEnumerable<ulong>
+    {
+        private ulong Value;
+
+        public BitArray64(ulong value = 0)
+        {
+            this.Value = value;
+        }
+
+        public IEnumerator<ulong> GetEnumerator()
+        {
+            for (int i = 63; i >= 0; i--)
+            {
+               yield return ((this.Value >> i) & 1);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        // Indexer
+        public ulong this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index <= 63)
+                {
+                    // Check the bit at position index
+                    return ((this.Value >> index) & 1);
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException(String.Format("Index {0} is invalid!", index));
+                }
+            }
+            set
+            {
+                if (index < 0 || index > 63)
+                {
+                    throw new IndexOutOfRangeException(String.Format("Index {0} is invalid!", index));
+                }
+
+                if (value < 0 || value > 1)
+                {
+                    throw new ArgumentException(String.Format("Value {0} is invalid!", value));
+                }
+
+                // Clear the bit at position index
+                this.Value &= ~(1ul << index);
+                // Set the bit at position index to value
+                this.Value |= (1ul << index);
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 63; i >= 0; i--)
+            {
+                result.Append((this.Value >> i) & 1);
+            }
+
+            return result.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var otherBitArray = obj as BitArray64;
+
+            if (otherBitArray != null)
+            {
+                for (int i = 0; i <= 63; i++)
+                {
+                    if (this[i] != otherBitArray[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator ==(BitArray64 a, BitArray64 b)
+        {
+            return BitArray64.Equals(a, b);
+        }
+
+        public static bool operator !=(BitArray64 a, BitArray64 b)
+        {
+            return !BitArray64.Equals(a, b);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode() ^ this.Value.GetHashCode();
+        }
+    }
+}
---
+++ week3/homework/Ex6/Ex6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitArray64 simpleBitArray = new BitArray64(9);
+
+            Console.WriteLine(simpleBitArray);
+
+            simpleBitArray[2] = 1;            
+            
+            //throws ex: simpleBitArray[65] = 1; 
+
+            Console.WriteLine(simpleBitArray);
+
+
+            Console.WriteLine(simpleBitArray[2] == simpleBitArray[3]);
+            
+            
+            Console.WriteLine(simpleBitArray[1] == simpleBitArray[3]);
+
+
+            foreach (var value in simpleBitArray)
+            {
+                Console.WriteLine(value);
+            }
+
+            BitArray64 otherBitArray = new BitArray64(4);
+            
+            BitArray64 latestBitArray = new BitArray64(13);
+
+            bool areEqual = simpleBitArray.Equals(otherBitArray);
+            Console.WriteLine("simpleBitArray == otherBitArray -> {0}", areEqual);            
+            
+            bool areEqual2 = simpleBitArray.Equals(latestBitArray);
+            Console.WriteLine("simpleBitArray == latestBitArray -> {0}", areEqual2);
+
+            Console.WriteLine(simpleBitArray.GetHashCode());
+
+        }
+    }
+}
